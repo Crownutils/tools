8a749c42e0097c0392ae7fc62ef2d976e171df5f
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2026-06-03 20:07:08 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/website/',
+  base: '/tools/',
   plugins: [
     vue(),
     vueDevTools(),
